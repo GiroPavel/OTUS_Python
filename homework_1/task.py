370502cb50731ab65fec5c5799bc49494d520bb0
Pavel <giropavel85@gmail.com>
2020-09-21 19:55:15 +0300

    Доработка

--- homework_1/task.py
+++ homework_1/task.py
@@ -6,19 +6,25 @@ n = range(11)
 
 print("Список целых чисел:", list(n))
 
-LEVEL = 0 # int
+sp = int(input("Введите степень:"))
+
+# LEVEL = 0 # int
 def trace(func):
     @wraps(func)
     def wrapper(*args):
-        global LEVEL
-        l = "--> " * LEVEL # str
+        # global LEVEL
+        # l = "--> " * LEVEL # str
+        w = "--> " * wrapper.step
         name = func.__name__
-        print(l + ("%s%s" % (name, args)))
-        LEVEL += 1
+        print(w + ("%s%s" % (name, args)))
+        # LEVEL += 1
+        wrapper.step += 1
         res = func(*args)
-        LEVEL -= 1
-        print(l + ("%s" % res))
+        # LEVEL -= 1
+        wrapper.step -= 1
+        print(w + ("%s" % res))
         return res
+    wrapper.step = 0
     return wrapper
 
 def timing_dec(func):
@@ -32,13 +38,21 @@ def timing_dec(func):
     return wrapper
 
 # 1
-def power (a, p=int(input("Введите степень:"))):
-    return a ** p
+def power_1(a, p=2):
+   return a ** p
+
+def power_2(a, p=sp):
+    return a ** sp
 
 @timing_dec
 def power_n():
-    res = map(power, n)
-    return res
+    if sp <= 2:
+        res = map(power_1, n)
+        return res
+    else:
+        res = map(power_2, n)
+        return res
+
 # 2
 def power_part_n():
     res = map(partial(pow, exp=2), n)
@@ -78,11 +92,5 @@ print("Простые числа в диапазоне от '0 до 10':", prime
 print()
 print("Вложенные входы в функцию на примере чисел Фибаначчи:")
 print("Список чисел Фибаначчи:", [fib(n) for n in range(5)])
-
-
-
-
-
-
-
-
+print()
+print()
