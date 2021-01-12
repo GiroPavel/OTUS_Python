74899988bcf7d4204504a5dd5f458c3c10445352
GiroPavel <51238453+GiroPavel@users.noreply.github.com>
2021-01-12 09:58:55 +0300

    Delete homework_7_django/clean_up directory

--- homework_7_django/clean_up/contact/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import ContactFormView, SuccessTemplateView
-
-app_name = 'contact'
-
-urlpatterns = [
-    path('', ContactFormView.as_view(), name='contact_form'),
-    path('success/', SuccessTemplateView.as_view(), name='contact_success'),
-]
--- homework_7_django/clean_up/project/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import ProjectTemplateView
-
-app_name = 'project'
-
-urlpatterns = [
-    path('', ProjectTemplateView.as_view(), name='project'),
-]
